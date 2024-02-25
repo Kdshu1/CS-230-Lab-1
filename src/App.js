2c8d4e6906bc0f23a477fc09a31b2648b2375ff4
your user name <your email>
2024-02-25 18:43:32 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return ( 
+    <div className="App">
+      <h1>CS 230L</h1>
+      <h2>Section - 003</h2>
+      <p>WVU ID: 800380069</p>
+      <p>Hi, I am Kyle Shumaker</p>
+  </div> 
+  );
+}
+
+export default App;
