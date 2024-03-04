0b771f67b560fce884e7b8d48f5345ea5addca50
your user name <your email>
2024-03-03 23:20:51 -0500

    fixes to card and navbar

--- src/Card.js
+++ src/Card.js
@@ -2,15 +2,18 @@ import React from 'react';
 
 const Card = () => {
   return (
-    <div className="row">
-      <div className="card" style={{ width: '18rem' }}>
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" className="card-link">Card link</a>
-          <a href="#" className="card-link">Another link</a>
-        </div>
+    <div className="card-container">
+      <div className="card">
+        <h2>Card 1</h2>
+        <p>This is the content of card 1.</p>
+      </div>
+      <div className="card">
+        <h2>Card 2</h2>
+        <p>This is the content of card 2.</p>
+      </div>
+      <div className="card">
+        <h2>Card 3</h2>
+        <p>This is the content of card 3.</p>
       </div>
     </div>
   );
--- src/Navbar.js
+++ src/Navbar.js
@@ -13,10 +13,10 @@ const Navbar = () => {
             <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Features</a>
+            <a className="nav-link" href="#">About</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Pricing</a>
+            <a className="nav-link" href="#">Contact</a>
           </li>
           <li className="nav-item">
             <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
